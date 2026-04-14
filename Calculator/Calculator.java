11fd52f84aa1527a8309123bfcba6b6a64be4928
Md Ashif Ansari <mdashifansari454@gmail.com>
2026-04-14 11:21:19 +0530

    "Task 1"

--- Calculator/Calculator.java
+++ Calculator/Calculator.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Calculator {
-    public static void main(String[] args) {
+    public static void main (String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
